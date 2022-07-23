a9094609502745bddef141af7e8aeb3f6ef548db
4bid-d <abidpponline@gmail.com>
2022-07-23 01:59:39 -0400

    added quatity feature in cart

--- public/javascripts/apply.js
+++ public/javascripts/apply.js
@@ -1,46 +1,52 @@
 
-function sendDeleteRequest(objectId) {
-  
-  function deleteconfirmation() {
-    
-    const confirmationText = "Are you sure to delete this item"
-    
-    if (confirm(confirmationText) == true) {
-        window.location.href = `/admin/delete/${objectId}`
-   
-    } 
+// function sendDeleteRequest(objectId) {
 
-  }
-  //function for delete a product after asking user top confirm
-  deleteconfirmation()
+//   function deleteconfirmation() {
+
+//     const confirmationText = "Are you sure to delete this item"
+
+//     if (confirm(confirmationText) == true) {
+//       window.location.href = `/admin/delete/${objectId}`
+
+//     }
+
+//   }
+//   //function for delete a product after asking user top confirm
+//   deleteconfirmation()
+// }
+
+
+// Converting JSON data to string
+var data = JSON.stringify({ "id": objectId, });
+console.log(data)
+// Sending data with the request
+xhr.send(data);
+
+function fetchData(id) {
+  const input = document.querySelector("#form1")
+
+  // let result = document.getElementById(objectId);
+  // let name = document.querySelector('#name');
+  // let email = document.querySelector('#email');
+  console.log(objectId)
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = `quantity/${id}/${input.value}`;
+
+  // open a connection
+  xhr.open("GET", url, true);
+
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Create a state change callback
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+
+      // Print received data from server
+      result.innerHTML = this.responseText;
+
+    }
+  };
+
 }
-
-   // let result = document.getElementById(objectId);
-      // let name = document.querySelector('#name');
-      // let email = document.querySelector('#email');
-      // console.log(objectId)
-      // Creating a XHR object
-      // let xhr = new XMLHttpRequest();
-      // let url = `/admin/delete/${objectId}`;
-
-      // // open a connection
-      // xhr.open("GET", url, true);
-
-      // Set the request header i.e. which type of content you are sending
-      // xhr.setRequestHeader("Content-Type", "application/json");
-
-      // Create a state change callback
-      // xhr.onreadystatechange = function () {
-      //   if (xhr.readyState === 4 && xhr.status === 200) {
-
-      //     // Print received data from server
-      //     result.innerHTML = this.responseText;
-
-      //   }
-      // };
-
-      // Converting JSON data to string
-      // var data = JSON.stringify({ "id": objectId, });
-      // console.log(data)
-      // // Sending data with the request
-      // xhr.send(data);
--- schemas/cartModel.js
+++ schemas/cartModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    cartKey:{
+ 
+    productId:{
         type:String,
-        immutable:true
+        immutable:true,
     },
-    productId:String,
+
     userEmail :{
         type:String,
         immutable:true,
@@ -16,7 +17,6 @@ const cartSchema = new mongoose.Schema({
         default:1
     }
 
-    
 })
 
 
